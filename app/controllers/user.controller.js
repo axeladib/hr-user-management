d67669262aa5cd11d012c19d144c63634ad00ec3
abbeyonde <114023807+abbeyonde@users.noreply.github.com>
2023-06-06 02:24:05 +0800

    Add files via upload

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -1,140 +1,83 @@
-const db = require("../models");
-const User = db.users;
-const Op = db.Sequelize.Op;
+const db = require('../models');
 
-// Create and Save a new User
-exports.create = (req, res) => {
-	// Validate request
-	if (!req.body.name) {
-		res.status(400).send({
-			message: "Content can not be empty!",
-		});
-		return;
-	}
+const users = {};
 
-	// Create a User
-	const user = {
-		name: req.body.name,
-		overallAttendance: req.body.overallAttendance,
-		latecomers: req.body.latecomers,
-		emergencyLeave: req.body.emergencyLeave,
-		outsideWork: req.body.outside.Work,
-		sickLeave: req.body.sickLeave,
-		annualLeave: req.body.annualLeave,
-	};
+//add new user
+users.addUser = (req,res) => {
+    //validate all required field
+    if(!req.body.username || !req.body.password || !req.body.email){
+        res.send("all fields are required!");
+        return;
+    }
+    const user = {
+        username: req.body.username,
+        password: req.body.password,
+        email: req.body.email,
+        employee: {
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            age: req.body.age,
+        }
+    }
+        
+    db.User.create(user,{include: [db.Employee]})
+    .then((data)=>{
+        res.send(data.username+' is successfully added to db');   
+    })
+    .catch((err)=>{
+        res.status(500).send(err.message || 'failed');
+        successful = false;
+    })
+;}
 
-	// Save User in the database
-	User.create(user)
-		.then((data) => {
-			res.send(data);
-		})
-		.catch((err) => {
-			res.status(500).send({
-				message: err.message || "Some error occurred while creating the User.",
-			});
-		});
+//get all users
+users.retrieveAll = (req,res) => {
+    //validate user, only admin has access
+    if(req.body.username !== 'admin' && req.body.password !== 'admin123'){
+        res.send('only admin has access to this page');
+        return;
+    }
+
+    db.User.findAll()
+    .then((data)=>{
+        res.send(data);
+    })
+    .catch((err)=>{
+        res.status(500).send(err.message || 'fail to retrieve');
+    })
+
+};
+//get one user
+users.retrieveUser = (req,res) => {
+
+    db.User.findOne({
+        where:{username: req.params.username},
+        include: db.Employee
+    })
+    .then((data)=>{
+        if(data){
+            res.send(data);
+        }
+        else{
+            res.status(404).send("error 404");
+        }
+    })
+    .catch(err=>{
+        res.status(500).send(err.message);
+    })
+
+};
+//edit user details
+users.updateUser = (req,res) => {
+
+};
+//delete one user
+users.delUser = (req,res) => {
+
+};
+//delete all user
+users.delAll = (req,res) => {
+
 };
 
-// Delete a User with the specified id in the request
-exports.delete = (req, res) => {
-	const id = req.params.id;
-
-	User.destroy({
-		where: { id: id },
-	})
-		.then((num) => {
-			if (num == 1) {
-				res.send({
-					message: "User was deleted successfully!",
-				});
-			} else {
-				res.send({
-					message: `Cannot delete User with id=${id}. Maybe User was not found!`,
-				});
-			}
-		})
-		.catch((err) => {
-			res.status(500).send({
-				message: "Could not delete User with id=" + id,
-			});
-		});
-};
-
-// Delete all User from the database.
-exports.deleteAll = (req, res) => {
-	User.destroy({
-		where: {},
-		truncate: false,
-	})
-		.then((nums) => {
-			res.send({ message: `${nums} Users were deleted successfully!` });
-		})
-		.catch((err) => {
-			res.status(500).send({
-				message:
-					err.message || "Some error occurred while removing all users.",
-			});
-		});
-};
-
-// Update a User by the id in the request
-exports.update = (req, res) => {
-	const id = req.params.id;
-
-	User.update(req.body, {
-		where: { id: id },
-	})
-		.then((num) => {
-			if (num == 1) {
-				res.send({
-					message: "User was updated successfully.",
-				});
-			} else {
-				res.send({
-					message: `Cannot update User with id=${id}. Maybe User was not found or req.body is empty!`,
-				});
-			}
-		})
-		.catch((err) => {
-			res.status(500).send({
-				message: "Error updating User with id=" + id,
-			});
-		});
-};
-
-// Find a single User by id
-exports.findOne = (req, res) => {
-	const id = req.params.id;
-
-	User.findByPk(id)
-		.then((data) => {
-			if (data) {
-				res.send(data);
-			} else {
-				res.status(404).send({
-					message: `Cannot find User with id=${id}.`,
-				});
-			}
-		})
-		.catch((err) => {
-			res.status(500).send({
-				message: "Error retrieving User with id=" + id,
-			});
-		});
-};
-
-// Find all User by name
-exports.findAll = (req, res) => {
-	const name = req.query.name;
-	var condition = name ? { name: { [Op.like]: `%${name}%` } } : null;
-
-	User.findAll({ where: condition })
-		.then((data) => {
-			res.send(data);
-		})
-		.catch((err) => {
-			res.status(500).send({
-				message: err.message || "Some error occurred while retrieving users.",
-			});
-		});
-};
+module.exports = users;
